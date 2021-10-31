4c0173cefac09962086ac2a9a63ec1c6768d25c2
DESKTOP-58F123L\hasin <hasinireddy765@gmail.com>
2021-10-31 02:03:47 -0700

    updated app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/',function(req,res){
-    res.send("Welcome to CMPE272 project-Team Nibble")
+    res.send("Welcome to CMPE-272 project :Team Nibble")
 })
 
 var server = app.listen(3000,function(){
