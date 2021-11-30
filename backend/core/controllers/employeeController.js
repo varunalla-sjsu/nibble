68b9123bf031a8b2bdc178fdcb1905c436b6fe1c
Varun Alla <varun.alla@sjsu.edu>
2021-11-30 11:43:23 -0800

    bug fix #1 employees controller error handling

--- backend/core/controllers/employeeController.js
+++ backend/core/controllers/employeeController.js
@@ -4,8 +4,8 @@ const userDataAccess=new (require('./../DataAccessObjects/employee'))(db);
 router.get('/',async (req,res)=>{
     //skip
     //limit
-    let skip=req.query.skip || 0;
-    let limit=req.query.limit || 20;
+    let skip=parseInt(req.query.skip) || 0;
+    let limit=parseInt(req.query.limit) || 20;
     try{
         let employees=await userDataAccess.getEmployees(skip,limit);
         return res.send(employees);
