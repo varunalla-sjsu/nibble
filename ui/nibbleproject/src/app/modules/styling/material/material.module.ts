e589e9416df83c25fab325a025e03ed986468db4
Varun Alla <varun.alla@sjsu.edu>
2021-11-30 13:18:07 -0800

    ui update

--- ui/nibbleproject/src/app/modules/styling/material/material.module.ts
+++ ui/nibbleproject/src/app/modules/styling/material/material.module.ts
@@ -1,12 +1,50 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTableModule} from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatListModule} from '@angular/material/list';
+import {MatCardModule} from '@angular/material/card';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatTooltipModule} from '@angular/material/tooltip';
 
+const materialModules=[
+  MatIconModule,
+  MatToolbarModule,
+  MatMenuModule,
+  MatButtonModule,
+  MatTableModule,
+  MatPaginatorModule,
+  MatSortModule,
+  MatSidenavModule,
+  MatDividerModule,
+  MatListModule,
+  MatCardModule,
+  MatDialogModule,
+  MatInputModule,
+  MatFormFieldModule,
+  MatSnackBarModule,
+  MatTooltipModule
+]
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    ...materialModules
+  ],
+  exports:[
+  ...materialModules
   ]
 })
 export class MaterialModule { }
