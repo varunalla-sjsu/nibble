dddedd259d53b075d8728a95e0a9a32d5aedda62
Varun Alla <varun.alla@sjsu.edu>
2021-11-30 10:53:24 -0800

    preprocessing middleware library

--- backend/core/server.js
+++ backend/core/server.js
@@ -2,10 +2,24 @@ const dotenv=require('dotenv');
 dotenv.config();
 const express = require('express');
 const app = express();
+const cors=require('cors');
 const auth=require('./auth/authMiddleware');
 const controllers=require('./controllers/index');
+/**
+ * Authentication Middleware
+ */
 app.use(auth.config);
 app.use(auth.middlware);
+/**
+ * cors 
+ * body parsing configuration 
+ * */
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded());
+/**
+ * Controller injection
+ *  */ 
 app.use('/',controllers);
 var server = app.listen(process.env.SERVER_PORT,function(){
     var port = server.address().port
