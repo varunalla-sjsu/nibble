4f279cdd6aaf5a8918dbd3a0e3d39bf8f3f79787
aakankshagupta1994 <aakanksha.gupta@sjsu.edu>
2021-11-30 16:36:37 -0800

    Added token details

--- backend/core/controllers/twitterhelperController.js
+++ backend/core/controllers/twitterhelperController.js
@@ -8,41 +8,69 @@ dotenv.config();
 // router methods..
 router.get('/', async (req, res) => {
 
+    let token = await reqAuthentication();
+    console.log(token);
+    res.redirect("https://api.twitter.com/oauth/authenticate?oauth_token=" + token.oauth_token);
+    console.log(token);
+
+});
+
+router.get('/auth', async (req, res) => {
+    console.log(res);
+    let token = await getToken(res); 
+    // res.send(res);
+   
+
+ 
+});
+
+
+async function getToken(res)
+{ 
+    const client = new twitterlite({
+        consumer_key: process.env.apiKey,
+        consumer_secret: process.env.apiSecret
+      });
+      
+      client
+        .getAccessToken({
+          oauth_verifier: res.oauthVerifier,
+          oauth_token: res.oauthToken
+        })
+        .then(res =>{
+            console.log("Reached token scope");
+          console.log({
+            accTkn: res.oauth_token,
+            accTknSecret: res.oauth_token_secret,
+            userId: res.user_id,
+            screenName: res.screen_name
+          })
+         } )
+        .catch(err => {
+            console.log(err);
+            // res.status(503).send({ "message": "could not process request" });
+        });
+}
+async function reqAuthentication() {
+   
     this.client = new twitterlite(
         {
             consumer_key: process.env.apiKey,
             consumer_secret: process.env.apiSecret
         }
     );
-    // let twitterObj = new TwitterService();
     let tokens = await this.client.getRequestToken("http://localhost:3000/twitter/auth")
-        .then(res => {
-            console.log(res);
-            res.redirect("https://api.twitter.com/oauth/authenticate?oauth_token=" + res.oauth_token);/*.then(res => {
-                this.client.getAccessToken({
-                    oauth_verifier: "",
-                    oauth_token: ""
-                }).then(res =>
-                    console.log({
-                        accTkn: res.oauth_token,
-                        accTknSecret: res.oauth_token_secret,
-                        userId: res.user_id,
-                        screenName: res.screen_name
-                    })
-                )
-                    .catch(console.error);
-            })
+        .then((res) => {
+            // console.log(res);
             return res;
 
-        })
-        .catch(console.error);
+        }).catch(err => {
+            console.log(err);
+            res.status(503).send({ "message": "could not process request" });
+        });
+    return tokens;
+}
 
-    console.log(tokens);
-    */}).catch(err=>{
-        res.status(503).send({"message":"could not process request"});
-    });
-
-});
 
 
 module.exports = router;
