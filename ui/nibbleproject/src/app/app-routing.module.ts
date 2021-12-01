21e6deecd17a56f12c984da2a761f61203f57ef9
ayadav1995 <abhishek.yadav02@sjsu.edu>
2021-11-30 23:54:39 -0800

    hrdshboard code in updated framework

--- ui/nibbleproject/src/app/app-routing.module.ts
+++ ui/nibbleproject/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { HrdashboardComponent } from './components/hrdashboard/hrdashboard.component';
 
 const routes: Routes = [
   {path:'',component:HomeComponent},
+  {path:'app-hrdashboard',component:HrdashboardComponent},
 ];
 
 @NgModule({
--- ui/nibbleproject/src/app/components/hrdashboard/hrdashboard.component.ts
+++ ui/nibbleproject/src/app/components/hrdashboard/hrdashboard.component.ts
@@ -1,4 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+
+// import * as Highcharts from 'highcharts';
+// import HC_exporting from 'highcharts/modules/HC_exporting';
+import { Component, OnInit,AfterViewInit, ViewChild } from '@angular/core';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+import {MatTableDataSource} from '@angular/material/table';
+import { UserService } from 'src/app/services/user.service';
+
+import {MatSnackBar} from '@angular/material/snack-bar';
+import { HrService } from 'src/app/services/hr.service';
+import { AllocationService } from 'src/app/services/allocation.service';
+
+
+export interface UserRequest {
+  employee: string;
+  raisedBy: string;
+  type: string;
+  requestid:string;
+  departmentid:string;
+
+}
+
+const ELEMENT_DATA: UserRequest[] = [
+];
+
+
 
 @Component({
   selector: 'app-hrdashboard',
@@ -7,9 +33,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HrdashboardComponent implements OnInit {
 
-  constructor() { }
+  departments : any;
+  
+  displayedColumns: string[] = ['employee', 'raisedby', 'type', 'departmentid','action'];
+  dataSource : MatTableDataSource<UserRequest>;
 
-  ngOnInit(): void {
+  
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  // displayedColumns: string[] = ['name','action'];
+  // dataSource: any;
+  constructor(private hrservice:HrService , private allocationservice:AllocationService ) {
+
+    this.dataSource = new MatTableDataSource(ELEMENT_DATA);
+   }
+
+
+  async ngOnInit() {
+    // this.dataSource.paginator = this.paginator;
+    this.departments = await this.hrservice.getDepartmentsInfo();
   }
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+
+    this.hrservice.getRequests().then((response:any)=>{
+      console.log(response);
+      this.dataSource=new MatTableDataSource<UserRequest>(response);
+    });
+    
+  }
+  refresh(){
+    this.hrservice.getRequests().then((response:any)=>{
+      console.log(response);
+      this.dataSource=new MatTableDataSource<UserRequest>(response);
+    });
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+  async action(username:string,status:string){
+    
+    console.log(username+' : '+status);
+    let resp=await this.allocationservice.approveReuest(username,status);
+    if(resp){
+     alert("request approved");
+    this.refresh();
+   }
+  }
+
+
 
 }
