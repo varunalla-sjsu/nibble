7634429368a0b13ac700c7b5e9eba376a723b78c
Varun Alla <varun.alla@sjsu.edu>
2021-12-01 11:22:24 -0800

    employee list with server side pagination

--- ui/nibbleproject/src/app/components/employees-list/employees-list.component.ts
+++ ui/nibbleproject/src/app/components/employees-list/employees-list.component.ts
@@ -28,7 +28,7 @@ export class EmployeesListComponent implements OnInit {
   paginator!: MatPaginator;
   constructor(private employeeService: EmployeesService) { }
   ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
+    //this.dataSource.paginator = this.paginator;
   }
 
   ngOnInit(): void {
@@ -39,35 +39,19 @@ export class EmployeesListComponent implements OnInit {
   loadData() {
     this.isLoading = true;
     this.employeeService.fetchUsers(this.currentPage * this.pageSize, this.pageSize).subscribe((result: any) => {
+      console.log(result);
       this.dataSource.data = result.rows;
-      setTimeout(() => {
 
-        this.totalRows = result.count;
+  //    this.paginator.length= result.count;
+      this.totalRows=result.count;
         this.paginator.pageIndex = this.currentPage;
-      });
-
+      
       this.isLoading = false;
     },
       err => {
         console.log('Error Fetching Data');
         this.isLoading = false;
       });
-    /*  let URL = `http://localhost/database.php?pageno=${this.currentPage}&per_page=${this.pageSize}`;
- 
- 
-     fetch(URL)
-       .then(response => response.json())
-       .then(data => {
-         this.dataSource.data = data.rows;
-         setTimeout(() => {
-           this.paginator.pageIndex = this.currentPage;
-           this.paginator.length = data.count;
-         });
-         this.isLoading = false;
-       }, error => {
-         console.log(error);
-         this.isLoading = false;
-       });*/
   }
 
   pageChanged(event: PageEvent) {
