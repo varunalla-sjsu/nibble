11d3e787fdba4171f32d0be0616b71983d78f950
aakankshagupta1994 <aakanksha.gupta@sjsu.edu>
2021-12-02 02:19:00 -0800

    post rating integration

--- backend/core/controllers/index.js
+++ backend/core/controllers/index.js
@@ -4,10 +4,11 @@ const homeRouter=require('./homeController');
 const employeeRouter=require('./employeeController');
 const departmentRouter=require('./departmentController');
 const twitterHelperRouter = require('./twitterhelperController');  // added by aakanksha: For calling twiiter api's
+const ratingRouter = require('./ratingsControllers'); // added by aakanksha : for getting rating records from table
 router.use('/home',homeRouter);
 router.use('/user',usersRouter);
 router.use('/employee',employeeRouter);
 router.use('/department',departmentRouter);
 router.use('/twitter', twitterHelperRouter);
-
+router.use('/ratings', ratingRouter);
 module.exports=router;
