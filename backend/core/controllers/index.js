314a9d8f6ce75c05184d215f6a2c0cea79cf672d
DESKTOP-58F123L\hasin <hasinireddy765@gmail.com>
2021-12-02 16:40:46 -0800

    Ui for usecase-allocation/deallocation

--- backend/core/controllers/index.js
+++ backend/core/controllers/index.js
@@ -4,9 +4,11 @@ const homeRouter=require('./homeController');
 const employeeRouter=require('./employeeController');
 const departmentRouter=require('./departmentController');
 const twitterHelperRouter = require('./twitterhelperController');  // added by aakanksha: For calling twiiter api's
+const makeRequest=require('./makeRequestController');
 router.use('/home',homeRouter);
 router.use('/user',usersRouter);
 router.use('/employee',employeeRouter);
 router.use('/department',departmentRouter);
 router.use('/twitter', twitterHelperRouter);
+router.use('/request',requestRouter);
 module.exports=router;
