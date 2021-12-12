ca23ee31c6f082c9ad7c6a66c62b3ceeb3d64e70
Varun Alla <varun.alla@sjsu.edu>
2021-12-12 00:43:12 -0800

    added the request back

--- backend/core/models/definitions/requests.js
+++ backend/core/models/definitions/requests.js
@@ -14,6 +14,9 @@ module.exports =
       type: DataTypes.STRING,
       allowNull: false,
     },
+    emp_no: {
+      type: DataTypes.INTEGER,
+    },
     req_dept: {
       type: DataTypes.STRING,
     },
